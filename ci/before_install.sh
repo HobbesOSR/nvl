e9547175f150b150461e704d75ae1b4158427a77
Noah Evans <noah.evans@gmail.com>
2016-03-25 08:54:06 -0600

    manually install syslinux and genisoimage to see if that will make build.pl work

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,4 +1,10 @@
-DEB=genisoimage_1.1.11-2ubuntu2_amd64.deb
+SLC=syslinux-common_4.05+dfsg-6+deb8u1_all.deb
+SL=syslinux_4.05+dfsg-6+deb8u1_amd64.deb
+GI=genisoimage_1.1.11-2ubuntu2_amd64.deb
+
 cd /tmp
-wget http://us.archive.ubuntu.com/ubuntu/pool/main/c/cdrkit/$DEB
-dpkg-deb --extract $DEB genisoimage
+wget http://us.archive.ubuntu.com/ubuntu/pool/main/c/cdrkit/$GI
+wget http://us.archive.ubuntu.com/ubuntu/pool/main/s/syslinux/$SLC
+wget http://us.archive.ubuntu.com/ubuntu/pool/main/s/syslinux/$SL
+sudo dpkg -i $GI $SLC $SL
+
