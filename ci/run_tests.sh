99022f3cc1bc28e7abba200379663a0ca2bb527a
Noah Evans <noah.evans@gmail.com>
2016-03-30 12:39:18 -0600

    now that ssh is (should be) running try running tests via ssh

---
+++ ci/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cssh() {
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $*
+}
+
+qemu-system-x86_64 -cdrom ./image.iso -m 2048 -smp 4 -serial stdio -monitor /dev/null -nographic -net nic -net user,hostfwd=tcp::2222-:22 &
+PID=$!
+sleep 20
+ssh-keyscan -t rsa,dsa localhost -p 2222 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
+mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
+cssh root@localhost -p 2222 echo worked
+cssh root@localhost -p 2222 halt
+# want a way to make qemu exit with the halt
+kill $PID
+exit 0
