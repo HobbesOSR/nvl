a72aa1a74a0ae4a98f31600e270e016a4176ac39
Noah Evans <noah.evans@gmail.com>
2016-04-13 16:11:12 +0000

    test console output for travis-ci

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -6,11 +6,12 @@ cssh() {
 
 qemu-system-x86_64 -cdrom ./image.iso -m 2048 -smp 4 -serial stdio -monitor /dev/null -nographic -net nic -net user,hostfwd=tcp::2222-:22 &
 PID=$!
-sleep 20
+sleep 40
 ssh-keyscan -t rsa,dsa localhost -p 2222 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
 mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
 cssh root@localhost -p 2222 /opt/tests/test_launch
-cssh root@localhost -p 2222 halt
+cssh root@localhost -p 2222 /opt/hobbes/hobbes console enclave-1
+#cssh root@localhost -p 2222 halt
 # want a way to make qemu exit with the halt
-kill $PID
+#kill $PID
 exit 0
